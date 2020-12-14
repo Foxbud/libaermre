0599e231ee93b6c3bc1d07eb4aebd19dd85a07ca
Foxbud <breadboardfox@gmail.com>
2020-12-14 16:10:38 -0500

    Documented mouse functions of mre module.

--- include/public/aer/mre.h
+++ include/public/aer/mre.h
@@ -3,6 +3,17 @@
  *
  * @brief Uncategorized functions for querying and manipulating game state.
  *
+ * @subsubsection MouseButtonStates Mouse Button States
+ *
+ * For functions that query information about the state of the mouse buttons,
+ * the following table shows how array indexes map to buttons.
+ *
+ * Array Index | Mouse Button
+ * ----------: | ------------
+ * 0           | Left
+ * 1           | Right
+ * 2           | Middle
+ *
  * @since 1.0.0
  */
 #ifndef AER_MRE_H
@@ -60,12 +71,59 @@ const bool * AERGetKeysHeld(void);
  */
 const bool * AERGetKeysReleased(void);
 
+/**
+ * @brief Query the mouse button(s) that the user just pressed this step.
+ *
+ * @return Array of mouse button pressed states. See @ref MouseButtonStates
+ * for more information.
+ *
+ * @throw ::AER_SEQ_BREAK if called outside action stage.
+ *
+ * @since 1.0.0
+ */
 const bool * AERGetMouseButtonsPressed(void);
 
+/**
+ * @brief Query the mouse button(s) that the user had held this step.
+ *
+ * @return Array of mouse button held states. See @ref MouseButtonStates
+ * for more information.
+ *
+ * @throw ::AER_SEQ_BREAK if called outside action stage.
+ *
+ * @since 1.0.0
+ */
 const bool * AERGetMouseButtonsHeld(void);
 
+/**
+ * @brief Query the mouse button(s) that the user just released this step.
+ *
+ * @return Array of mouse button released states. See @ref MouseButtonStates
+ * for more information.
+ *
+ * @throw ::AER_SEQ_BREAK if called outside action stage.
+ *
+ * @since 1.0.0
+ */
 const bool * AERGetMouseButtonsReleased(void);
 
+/**
+ * @brief Query the current position of the mouse cursor in pixels.
+ *
+ * If only one component of the position is needed, then the argument for the
+ * unneeded component may be `NULL`.
+ *
+ * @note This function returns coordinates relative to the game window,
+ * not the current room.
+ *
+ * @param[out] x Horizontal position.
+ * @param[out] y Vertical position.
+ *
+ * @throw ::AER_SEQ_BREAK if called outside action stage.
+ * @throw ::AER_NULL_ARG if both arguments `x` and `y` are `NULL`.
+ *
+ * @since 1.0.0
+ */
 void AERGetMousePosition(
 		uint32_t * x,
 		uint32_t * y
