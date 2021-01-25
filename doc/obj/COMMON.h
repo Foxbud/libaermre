b979f965d94ce6e2ff7df013640114a99382195f
bloxar <cdg314729@gmail.com>
2021-01-25 12:46:25 +1000

    Added object docs
    - Common locals for all object types
    - Locals for BULLET object

---
+++ doc/obj/COMMON.h
@@ -0,0 +1,44 @@
+/**
+ * @page CommonLocals Common Locals
+ * 
+ * t (double): Time since the instance was created.
+ * xc
+ * yc
+ * z (double): Current position on the z-axis.
+ * xspd (double): Current speed on the x-axis.
+ * yspd (double): Current speed on the y-axis.
+ * zspd (double): Current speed on the z-axis.
+ * xDrag
+ * yDrag
+ * 
+ * caseState 
+ * caseTime 
+ * caseDelayDir 
+ * caseCheck
+ * caseArg
+ * caseDelay
+ * caseScript
+ * 
+ * selected
+ * editorType
+ * editorSpawner
+ * editorSubType
+ * editorHidden
+ * showScriptingMenu
+ * layer
+ * 
+ * muteObject (bool): All sounds are muted if >=1
+ * lowAudioEffect
+ * 
+ * cid (uint): An ID (unique within the room) for a specific instance.
+ * freeze
+ * pause
+ * controlsHaveBeenSet
+ * deactivateInstance
+ * objWindow
+ * inherited
+ * linkArg 
+ * linkID
+ * justcreated 
+ * usesCaseChecking 
+ */
---
+++ doc/obj/DOOR.h
@@ -0,0 +1,18 @@
+/**
+ * @page ObjPageDoor Door Object
+ *
+ *
+ * Locals:
+ *
+ * doorgoto (uint): The cid of the door in the destination room.
+ * roomgoto (uint): The ID of the room this door leads to.
+ * exitDir (double): The drifter's facing angle when coming out of this door, in degrees.
+ * walkOut (bool): Whether the drifter walks into the room.
+ * lastRoomgoto : ?
+ * roomgotoBG : ?
+ * roomgotoDoors : ?
+ * roomgotoW : ?
+ * roomgotoH : ?
+ * justExited : ?
+ * cued : ?
+ */
